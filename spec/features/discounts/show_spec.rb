f7a8bbb613022615c6a75a5b78649ae598dd94cd
Bradley Milton <bmilton2412@gmail.com>
2023-08-04 15:54:27 -0700

    add discounts show spec with test, add discount show to controller, add discount show view page

--- spec/features/discounts/show_spec.rb
+++ spec/features/discounts/show_spec.rb
@@ -1,10 +1,25 @@
-# require "rails_helper"
+require "rails_helper"
 
-# describe "merchant discounts show page" do
-#   before :each do
-#     @merchant1 = Merchant.create!(name: "Hair Care")
-#     @discount1 = Discount.create!(percentage_discount: 15, quantity_threshold: 10, merchant_id: @merchant1.id)
-#   end
+describe "merchant discounts show page" do
+  before :each do
+    @merchant1 = Merchant.create!(name: "Hair Care")
+    @discount1 = Discount.create!(percentage_discount: 15, quantity_threshold: 10, merchant_id: @merchant1.id)
+    @discount2 = Discount.create!(percentage_discount: 25, quantity_threshold: 15, merchant_id: @merchant1.id)
+  end
 
-#   it "should show the discount
-# end
+  it "should show discount quantity threshold and percentage discount" do
+    visit merchant_discount_path(@merchant1, @discount1)
+
+    expect(page).to have_content("Discount: 1")
+
+    expect(page).to have_content("Quantity Threshold: 10")
+    expect(page).to have_content("Percentage Discount: 15")
+  end
+end
+
+
+# 4: Merchant Bulk Discount Show
+
+# As a merchant
+# When I visit my bulk discount show page
+# Then I see the bulk discount's quantity threshold and percentage discount
