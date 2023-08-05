cd8262a8adbbe2ca340489050ac6c99fa195e74b
Bradley Milton <bmilton2412@gmail.com>
2023-08-05 11:48:20 -0700

    add link to edit discount to discount show, add for to edit a discount with pre populated info, add redirect messages, add test for discount edit

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -29,6 +29,24 @@ class DiscountsController < ApplicationController
     end
   end
 
+  def edit
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = @merchant.discounts.find(params[:id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = @merchant.discounts.find(params[:id])
+
+      if @discount.update(discount_params)
+        redirect_to merchant_discount_path(@merchant, @discount)
+        flash[:success] = "Discount was successfully updated"
+      else
+        redirect_to edit_merchant_discount_path(@merchant, @discount)
+        flash[:error] = "Please fill in all fields"
+      end
+  end
+
   def destroy
     @merchant = Merchant.find(params[:merchant_id])
     @discount = @merchant.discounts.find(params[:id])
---
+++ spec/features/discounts/edit_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+
+RSpec.describe "merchant discounts edit page" do
+  before :each do
+    @merchant1 = Merchant.create!(name: "Hair Care")
+    @discount1 = Discount.create!(percentage_discount: 15, quantity_threshold: 10, merchant_id: @merchant1.id)
+  end
+
+  # US 5
+  it "should have a form with pre populated attributes for discount, once attributs are changed, takes you back to that discounts show page" do
+    visit edit_merchant_discount_path(@merchant1, @discount1)
+    # save_and_open_page
+    expect(page).to have_content("Edit Discount: #{@discount1.id}")
+    expect(page).to have_content("Quantity Threshold: 10")
+    expect(page).to have_content("Percentage Discount: 15")
+
+    expect(page).to have_field("Quantity threshold", with: "10")
+    fill_in("Quantity threshold", with: "12")
+    expect(page).to have_field("Percentage discount", with: "15")
+
+    click_button "Submit"
+
+    expect(current_path).to eq(merchant_discount_path(@merchant1, @discount1))
+    expect(page).to have_content("Quantity Threshold: 12")
+    expect(page).to_not have_content("Quantity Threshold: 10")
+    end
+
+    it "displays a flash message 'Please fill in all fields' when data entry is invalid" do
+      visit edit_merchant_discount_path(@merchant1, @discount1)
+  
+      fill_in("Quantity threshold", with: "")
+      fill_in("Percentage discount", with: "")
+      click_button('Submit')
+  
+      expect(current_path).to eq(edit_merchant_discount_path(@merchant1, @discount1))
+      expect(page).to have_text("Please fill in all fields")
+    end
+end
+
+
+
